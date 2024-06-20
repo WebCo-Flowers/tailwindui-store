c9369de3c56d49b7b7fecfe20a460d774bfb7618
Mohit Thakrar <mohitthakkar@gmail.com>
2024-06-20 19:04:11 +0530

    Florist's Choice Daily Deal Destop & Responsive Continue

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     /* **** End Add Remove Class **** */
 
     /* **** Hero Slider **** */
-    $('.hero-slider').slick({
+    $(".hero-slider").slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -17,7 +17,7 @@ $(document).ready(function () {
         autoplaySpeed: 3000,
         speed: 1000,
         fade: true,
-        cssEase: 'linear',
+        cssEase: "linear",
         prevArrow: "<button type='button' class='slick-prev slick-arrow'><i class='fal fa-chevron-left'></i></button>",
         nextArrow: "<button type='button' class='slick-next slick-arrow'><i class='fal fa-chevron-right'></i></button>",
         responsive: true,
@@ -26,14 +26,14 @@ $(document).ready(function () {
                 breakpoint: 991,
                 settings: {
                     arrows: false,
-                }
-            }
-        ]
+                },
+            },
+        ],
     });
     /* **** End Hero Slider **** */
 
     /* **** Hero Slider **** */
-    $('.fresh-slider').slick({
+    $(".fresh-slider").slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -48,19 +48,19 @@ $(document).ready(function () {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                }
+                },
             },
             {
                 breakpoint: 991,
                 settings: {
                     slidesToShow: 3,
-                }
+                },
             },
             {
                 breakpoint: 640,
                 settings: {
                     slidesToShow: 2,
-                }
+                },
             },
             {
                 breakpoint: 575,
@@ -69,7 +69,7 @@ $(document).ready(function () {
                     arrows: false,
                     centerPadding: "210px 0 0",
                     slidesToShow: 1,
-                }
+                },
             },
             {
                 breakpoint: 447,
@@ -78,14 +78,14 @@ $(document).ready(function () {
                     arrows: false,
                     centerPadding: "150px 0 0",
                     slidesToShow: 1,
-                }
-            }
-        ]
+                },
+            },
+        ],
     });
     /* **** End Hero Slider **** */
 
     /* **** Hero Slider **** */
-    $('.products-slider').slick({
+    $(".products-slider").slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -100,19 +100,19 @@ $(document).ready(function () {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                }
+                },
             },
             {
                 breakpoint: 991,
                 settings: {
                     slidesToShow: 3,
-                }
+                },
             },
             {
                 breakpoint: 767,
                 settings: {
                     slidesToShow: 2,
-                }
+                },
             },
             {
                 breakpoint: 575,
@@ -121,7 +121,7 @@ $(document).ready(function () {
                     arrows: false,
                     centerPadding: "210px 0 0",
                     slidesToShow: 1,
-                }
+                },
             },
             {
                 breakpoint: 447,
@@ -130,9 +130,9 @@ $(document).ready(function () {
                     arrows: false,
                     centerPadding: "150px 0 0",
                     slidesToShow: 1,
-                }
-            }
-        ]
+                },
+            },
+        ],
     });
     /* **** End Hero Slider **** */
 
@@ -155,4 +155,52 @@ $(document).ready(function () {
         variableWidth: true,
     });
     /* **** End Product Slider **** */
+
+    /* ***** Quantity Inputs **** */
+    $(".quantity").on("click", ".plus", function (e) {
+        let $input = $(this).prev("input.qty");
+        let val = parseInt($input.val());
+        $input.val(val + 1).change();
+    });
+
+    $(".quantity").on("click", ".minus", function (e) {
+        let $input = $(this).next("input.qty");
+        var val = parseInt($input.val());
+        if (val > 0) {
+            $input.val(val - 1).change();
+        }
+    });
+    /* ***** End Quantity Inputs **** */
+
+    /* **** Fix Header **** */
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 218) {
+            $("header").addClass("sticky");
+        } else {
+            $("header").removeClass("sticky");
+        }
+    });
+    /* **** End Fix Header **** */
+
+    /* **** Tabs **** */
+    let tabsContainer = document.querySelector("#tabs");
+    let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
+    console.log(tabTogglers);
+    tabTogglers.forEach(function (toggler) {
+        toggler.addEventListener("click", function (e) {
+            e.preventDefault();
+            let tabName = this.getAttribute("href");
+            let tabContents = document.querySelector("#tab-contents");
+            for (let i = 0; i < tabContents.children.length; i++) {
+                tabTogglers[i].parentElement.classList.remove("border-darker-foliage", "text-darker-foliage", "border-transparent");
+                tabContents.children[i].classList.remove("hidden");
+                if ("#" + tabContents.children[i].id === tabName) {
+                    continue;
+                }
+                tabContents.children[i].classList.add("hidden");
+            }
+            e.target.parentElement.classList.add("border-darker-foliage", "text-darker-foliage");
+        });
+    });
+    /* **** End Tabs **** */
 });
