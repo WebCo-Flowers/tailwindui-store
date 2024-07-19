7bbf98f90ee4a647057ce6ed17407ff2615dd262
Mohit Thakrar <mohitthakkar@gmail.com>
2024-07-19 19:35:52 +0530

    Doc point 4 and 6 done, point 5 is continue

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      libre: 'var(--font-libre)'
+    },
+    extend: {
+      colors: {
+        'vine-lighter': 'var(--vine-lighter)',
+        'foliage': 'var(--foliage)',
+        'petal': 'var(--petal)',
+        'sage': 'var(--sage)',
+        'stone': 'var(--stone)',
+        'darker-foliage': 'var(--darker-foliage)',
+        'light-foliage': 'var(--light-foliage)',
+        'body-text': 'var(--body-text)',
+        'light-body-text': 'var(--light-body-text)',
+        'divider-outline': 'var(--divider-outline)',
+      }
+    }
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
+
